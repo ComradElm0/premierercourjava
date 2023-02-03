c0cdab8612007fe42cd08c7ce5e1d85c80d9b565
ecorb <e.corbeel@yahoo.fr>
2023-02-03 13:33:36 +0100

    Début Tp4

--- src/tp4/Tp4.java
+++ src/tp4/Tp4.java
@@ -1,5 +1,10 @@
 package tp4;
 
+import javax.imageio.IIOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Tp4 {
 	
 	public static void main(String[] args){
@@ -7,7 +12,27 @@ public class Tp4 {
 		demo.run();
 	}
 	private void run(){
-	
+		FileWriter mywriter =null;
+		try {
+			File myfile = new File("C:\\Users\\ecorb\\Documents");
+			if (!myfile.exists()){
+				myfile.createNewFile();
+			}
+			mywriter = new FileWriter(myfile);
+			mywriter.write(("Hello from code!"));
+			mywriter.flush();
+		}catch (IOException e){
+			System.out.println(e.getMessage());
+		} finally {
+			if (mywriter != null){
+				try{
+					mywriter.close();
+				}catch (IOException e){
+					System.out.println(e.getMessage());
+				}
+			}
+			
+		}
 	}
 }
 
